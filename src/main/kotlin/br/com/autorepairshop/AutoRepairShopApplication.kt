65071d85bea4c9c5f1ea6b46d90118e7dec51e39
Henrique Marques <hmdarkfire.dev@gmail.com>
2026-08-23 12:33:30 -0300

    chore: first template

---
+++ src/main/kotlin/br/com/autorepairshop/AutoRepairShopApplication.kt
@@ -0,0 +1,11 @@
+package br.com.autorepairshop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutoRepairShopApplication
+
+fun main(args: Array<String>) {
+    runApplication<AutoRepairShopApplication>(*args)
+}
